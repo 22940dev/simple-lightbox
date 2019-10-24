e70c8985ea4b6318254f906001703dee2b0ec055
Jarosław Terejko <j.terejko@gmail.com>
2019-10-24 09:55:27 +0200

    added close btn to lightbox img

--- app.js
+++ app.js
@@ -1,23 +1,35 @@
-const lightbox = document.createElement('div')
-lightbox.setAttribute("id", "lightbox")
-document.body.append(lightbox)
+const lightbox = document.createElement("div");
+lightbox.setAttribute("id", "lightbox");
+document.body.append(lightbox);
 
-const imgs = document.querySelectorAll('.grid img')
-console.log(imgs)
+const lightboxImgBtn = document.createElement("button");
+lightboxImgBtn.innerHTML = "&times";
+
+const imgs = document.querySelectorAll(".grid img");
+console.log(imgs);
 
 imgs.forEach(img => {
-    img.addEventListener('click', e => {
-        lightbox.classList.add('active')
-        const imgLightbox = document.createElement('img')
-        imgLightbox.setAttribute("src", `${img.src}`)
-        if(lightbox.firstChild) {
-            lightbox.firstChild.remove()
-        }
-        lightbox.append(imgLightbox)
-    })
-})
+  img.addEventListener("click", e => {
+    lightbox.classList.add("active");
+    const imgLightbox = document.createElement("img");
+    imgLightbox.setAttribute("src", `${img.src}`);
+    const lightboxImgCnt = document.createElement("div");
+   
+    while (lightbox.firstChild) {
+      lightbox.removeChild(lightbox.firstChild);
+    }
 
-lightbox.addEventListener("click", e=> {
-    if(e.target !== e.currentTarget) return
-    lightbox.classList.remove('active')
-})
+    lightbox.appendChild(lightboxImgCnt);
+    lightboxImgCnt.appendChild(imgLightbox);
+    lightboxImgCnt.appendChild(lightboxImgBtn);
+  });
+});
+
+lightbox.addEventListener("click", e => {
+  if (e.target !== e.currentTarget) return;
+  lightbox.classList.remove("active");
+});
+
+lightboxImgBtn.addEventListener("click", e => {
+  lightbox.classList.remove('active')
+});

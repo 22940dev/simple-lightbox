88fed264f596c0e38632df37df4d2af359f37696
Jarosław Terejko <j.terejko@gmail.com>
2019-10-25 08:33:11 +0200

    added margin to lightbox

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const lightbox = document.createElement("div");
 lightbox.setAttribute("id", "lightbox");
-document.body.append(lightbox);
+document.body.appendChild(lightbox);
 
 const lightboxImgBtn = document.createElement("button");
 lightboxImgBtn.innerHTML = "&times";
@@ -11,11 +11,15 @@ console.log(imgs);
 imgs.forEach(img => {
   img.addEventListener("click", e => {
     lightbox.classList.add("active");
+
     const imgLightbox = document.createElement("img");
+    console.log(img);
     imgLightbox.setAttribute("src", `${img.src}`);
+
     const lightboxImgCnt = document.createElement("div");
-   
+
     while (lightbox.firstChild) {
+      console.log(lightbox.firstChild);
       lightbox.removeChild(lightbox.firstChild);
     }
 
@@ -26,10 +30,11 @@ imgs.forEach(img => {
 });
 
 lightbox.addEventListener("click", e => {
+  console.log(e.target);
   if (e.target !== e.currentTarget) return;
   lightbox.classList.remove("active");
 });
 
 lightboxImgBtn.addEventListener("click", e => {
-  lightbox.classList.remove('active')
+  lightbox.classList.remove("active");
 });
